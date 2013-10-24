7f106ddb0e909a11d85fa0edf949d864656d1d5d
Varand Abrahamian <Zelsoft@hotmail.com>
2013-10-23 20:36:31 -0700

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitch::Application.config.secret_token = '4eda309510b897ade5ce623c3cb910dda97bd393fc4e0df77d7b2d112c35e2090ccf3e21177fb6afbb7a164bf2da80f09ac258adabcfbf0f9b0cfe0d1ad522b4'
